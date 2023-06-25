1e8324e22dc8ef3620f66675f213e3b31347be5e
Fernanda Santos <santo4fer@gmail.com>
2023-06-25 19:58:55 -0300

    adc páginas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,29 @@
+import Footer from 'components/Footer';
 import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
+import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRoputer() {
    return (
-      <Router>
-         <Menu />
-         <Routes>
-            <Route path='/' element={<Inicio />} />
-            <Route path='/cardapio' element={<Cardapio />} />
-         </Routes>
-      </Router>
-   )
+      <main className='container'>
+         <Router>
+            <Menu />
+            <Routes>
+               <Route path='/' element={<PaginaPadrao />}>
+                  <Route index element={<Inicio />} />
+                  <Route path='cardapio' element={<Cardapio />} />
+                  <Route path='sobre' element={<Sobre />} />
+                  <Route path='prato/:id' element={<Prato />} />
+               </Route>
+               <Route path='*' element={<NotFound />} />
+            </Routes>
+            <Footer />
+         </Router>
+      </main>
+   );
 }
